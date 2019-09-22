41e1e2decb13f1b50381563e3d41ef6fa2954362
EadingWen <41257189+EadingWen@users.noreply.github.com>
2019-09-22 16:48:00 +0800

    Add files via upload

---
+++ 短网址/shortURL.js
@@ -0,0 +1,43 @@
+
+function getShortURL()
+{
+	var ajax = new XMLHttpRequest();
+    var token = '7dbaed9e2588ec21bfa6a433695bfe8f';
+    var longUrl = document.getElementById("longURL").value;
+    var termOfValidity = '1-year';
+
+    ajax.open('post','https://dwz.cn/admin/v2/create', 'true');
+
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.setRequestHeader("Token", token);
+    
+    // 发送请求
+    ajax.send(JSON.stringify({
+        Url: longUrl,
+        TermOfValidity: termOfValidity
+    }));
+
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4 && ajax.status === 200) {
+            // 获取缩短后的网址
+			var str = ajax.responseText;
+			
+			if(str.search('{"Code":0,"ShortUrl":"') == 0)
+			{
+				str = str.replace('{"Code":0,"ShortUrl":"','');
+				str = str.replace(/".*}/,'');
+				//alert("短网址生成成功：" + str);
+				document.getElementById("Alert").innerHTML = "短网址生成成功：";
+				document.getElementById("shortURL").href = str ;
+				document.getElementById("shortURL").innerHTML = str ;
+			}
+			else
+			{
+				//alert('暂不支持生成该网址的短网址！');
+				document.getElementById("Alert").innerHTML = "暂不支持生成该网址的短网址！";
+				document.getElementById("shortURL").href = '' ;
+				document.getElementById("shortURL").innerHTML = '' ;
+			}
+        }
+    }
+}
---
+++ 短网址/shortURL示例.js
@@ -0,0 +1,33 @@
+    var ajax = new XMLHttpRequest();
+    var token = '7dbaed9e2588ec21bfa6a433695bfe8f';
+    var longUrl = 'https://www.bilibili.com/';
+    var termOfValidity = '1-year'
+
+    ajax.open('post','https://dwz.cn/admin/v2/create', 'true');
+
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.setRequestHeader("Token", token);
+    
+    // 发送请求
+    ajax.send(JSON.stringify({
+        Url: longUrl,
+        TermOfValidity: termOfValidity
+    }));
+
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4 && ajax.status === 200) {
+            // 获取缩短后的网址
+			var str = ajax.responseText;
+			
+			if(str.search('{"Code":0,"ShortUrl":"') == 0)
+			{
+				str = str.replace('{"Code":0,"ShortUrl":"','');
+				str = str.replace(/".*}/,'');
+				alert("短网址生成成功：" + str);
+			}
+			else
+			{
+				alert('暂不支持生成该网址的短网址！');
+			}
+        }
+    }
